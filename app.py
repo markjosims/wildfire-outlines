659fa357c41474183400c4240956cf5501d45574
Mark J Simmons <mjsimmons@ucsd.edu>
2026-03-29 19:41:12 -0700

    improve conversation structure and flow

--- app.py
+++ app.py
@@ -2,39 +2,92 @@
 Based off demo in https://docs.streamlit.io/develop/tutorials/chat-and-llm-apps/build-conversational-apps
 """
 
+from re import A
 import streamlit as st
-from chat import get_system_prompt, get_model_response, QuestionServer
+from chat import (
+    QuestionServer,
+    handle_assistant_greeting,
+    handle_next_question,
+    handle_student_response,
+    handle_assistant_response,
+)
+from outlines.inputs import Chat
+from typing import Optional, Literal
 
 st.title("Wildfire demo assessment")
 
 
-@st.cache_resource
 def get_question_server():
-    return QuestionServer()
+    if "question_server" in st.session_state:
+        return st.session_state.question_server
+    question_server = QuestionServer()
+    st.session_state.question_server = question_server
+    return question_server
 
 
-question_server = get_question_server()
+def get_chat():
+    if "chat" in st.session_state:
+        return st.session_state.chat
+    chat = Chat()
+    chat = handle_assistant_greeting(chat, st.session_state.question_server)
+    st.session_state.chat = chat
+    return chat
 
-first_message = False
-if "messages" not in st.session_state:
-    messages = []
-    st.session_state.messages = get_system_prompt(messages)
-    first_message = True
 
-for message in st.session_state.messages:
+def get_user_response_type() -> Optional[Literal["Answer", "Ask for clarification"]]:
+    chat: Chat = st.session_state.chat
+    question_server: QuestionServer = st.session_state.question_server
+    question_status = question_server.get_question_status()
+    if question_status == "attempts_and_clarifications":
+        user_response_type = st.pills(
+            label="Response type", options=["Answer", "Ask for clarification"]
+        )
+    elif question_status == "no_clarifications":
+        user_response_type = st.pills(
+            label="Response type", options=["Answer"], default="Answer"
+        )
+    elif question_status == "no_attempts":
+        handle_next_question(chat, question_server)
+        user_response_type = None
+        st.rerun()
+    else:
+        raise ValueError(f"Unrecognized question status", question_status)
+    return user_response_type
+
+
+get_question_server()
+get_chat()
+
+# print all non-system messages to chat
+for message in st.session_state.chat.messages:
     if message["role"] == "system":
         continue
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Type response here"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+# allow user to choose between "answer" and "ask for clarification"
+# based on previous question
+user_response_type = get_user_response_type()
+
+# get user's response to assistant's last message
+# user must select response type before chat will be enabled
+if prompt := st.chat_input("Type response here", disabled=not user_response_type):
+    # prepend response type to prompt for transparency when printed to chat
+    prompt = f"({user_response_type}) {prompt}"
+
+    chat = st.session_state.chat
+    question_server = st.session_state.question_server
+    handle_student_response(chat, user_response_type, question_server, prompt)
 
     with st.chat_message("user"):
         st.markdown(prompt)
 
-if prompt or first_message:
-    messages = st.session_state.messages
-    with st.chat_message("assistant"):
-        messages = get_model_response(messages, question_server, st.write)
-    st.session_state.messages = messages
+# give user prompt to assistant and let assistant decide to follow up
+# or ask next question
+if prompt:
+    chat = st.session_state.chat
+    chat = handle_assistant_response(chat, st.session_state.question_server)
+
+    st.session_state.chat = chat
+    # rerun app so messages will be printed, as handled above
+    st.rerun()
--- chat.py
+++ chat.py
@@ -14,6 +14,8 @@ import os
 from typing import Literal
 import logging
 
+from streamlit.delta_generator import Value
+
 load_dotenv()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -30,6 +32,16 @@ JSON_PATH = "./data/wildfire_questions_B.json"
 
 ADVANCE_TYPE = Literal["next_question", "next_chapter", "end_test"]
 
+QUESTION_TEMPLATE = """
+## Concept: {concept_description}
+**Type:** {question_format}
+**Question:** {question_text}
+
+You may ask {max_clarifications} clarification questions
+and you have {max_answer_attempts} attempts to answer correctly
+before the assessment will automatically progress to the next question.
+"""
+
 
 class QuestionServer:
     def __init__(self, json_path: str = JSON_PATH) -> None:
@@ -39,12 +51,23 @@ class QuestionServer:
         # chapter index corresponds to chapter number in course textbook
         # and so it is 1-indexed
         # question index corresponds to index in JSON array
-        # and so it is 0-indexed
+        # and so it is 0-indexed, but starts at -1 since
+        # 'handle_next_question' always increments question_index
+        # at the beginning
         self.chapter_index = 1
-        self.question_index = 0
+        self.question_index = -1
         self.max_chapter = max(
             int(chapter_data["chapter"]) for chapter_data in self.data
         )
+
+        # to prevent the student getting stuck on a single question,
+        # we allow n=5 clarification questions and m=5 answer attempts,
+        # then we automatically progress to the next question
+        self.num_clarifications = 0
+        self.num_answer_attempts = 0
+
+        self.max_clarifications = 5
+        self.max_answer_attempts = 5
 
     def load_data(self) -> list[dict[str, str | dict[str, str]]]:
         with open(self.json_path) as f:
@@ -62,7 +85,39 @@ class QuestionServer:
         assert len(chapter_data) == 1
         return chapter_data[0]
 
-    def get_current_question(self) -> dict[str, str]:
+    def increment_clarifications(self):
+        self.num_clarifications = self.num_clarifications + 1
+
+    def increment_attempts(self):
+        self.num_answer_attempts = self.num_answer_attempts + 1
+
+    def remaining_clarifications(self) -> int:
+        return self.max_clarifications - self.num_clarifications
+
+    def remaining_attempts(self) -> int:
+        return self.max_answer_attempts - self.num_answer_attempts
+
+    def get_attempt_and_clarification_message(self) -> str:
+        remaining_attempts = self.remaining_attempts()
+        remaining_clarifications = self.remaining_clarifications()
+        if remaining_attempts <= 0:
+            return "Max answer attempts reached for this question!"
+
+        if remaining_clarifications <= 0:
+            return f"Max clarification questions reached. {remaining_attempts} answer attempts remain."
+
+        return f"There are {remaining_clarifications} clarification questions and {remaining_attempts} answer attempts remaining for this question."
+
+    def get_question_status(
+        self,
+    ) -> Literal["attempts_and_clarifications", "no_clarifications", "no_attempts"]:
+        if self.remaining_attempts() <= 0:
+            return "no_attempts"
+        if self.remaining_clarifications() <= 0:
+            return "no_clarifications"
+        return "attempts_and_clarifications"
+
+    def get_current_question_data(self) -> dict[str, str]:
         chapter_data = self.get_current_chapter_data()
         question_data: dict[str, str] = chapter_data["questions"][self.question_index]
         question_data = {
@@ -72,16 +127,28 @@ class QuestionServer:
         }
         return question_data
 
+    def format_question(self, **question_data) -> str:
+        question_str = QUESTION_TEMPLATE.format(
+            max_clarifications=self.max_clarifications,
+            max_answer_attempts=self.max_answer_attempts,
+            **question_data,
+        )
+        return question_str
+
     def advance_question(self) -> ADVANCE_TYPE:
         """
         Advance to the next question within the chapter if available.
         If at the end of the chapter, advance to the next chapter instead,
         and if at end of last chapter, return 'end_test'.
         """
+
+        self.num_answer_attempts = 0
+        self.num_clarifications = 0
+
         question_index = self.question_index + 1
 
         chapter_data = self.get_current_chapter_data()
-        chapter_num_questions = len(chapter["questions"])
+        chapter_num_questions = len(chapter_data["questions"])
         if question_index >= chapter_num_questions:
             # advance to next chapter and reset question index
             self.question_index = 0
@@ -100,15 +167,14 @@ Structured response types
 """
 
 
+class Greeting(BaseModel):
+    message: str
+
+
 class Response(BaseModel):
-    content: str
-    comment: str
+    message: str
+    reasoning: str
     decision: Literal["follow_up", "next_question"]
-
-
-class Question(BaseModel):
-    content: str
-    comment: str
 
 
 """
@@ -116,52 +182,110 @@ Prompt functions
 """
 
 
-def get_system_prompt(messages: list[dict[str, str]]) -> list[dict[str, str]]:
-    system_prompt_path = "./system-prompt.txt"
+def get_system_prompt(
+    role: Literal["assistant", "student"] = "assistant",
+    prompt_type: Literal["initial", "question", "clarify", "answer"] = "initial",
+) -> str:
+
+    system_prompt_path = f"./prompts/{role}/{prompt_type}-prompt.txt"
     with open(system_prompt_path) as f:
         system_prompt = f.read()
-    messages.append(
-        {
-            "role": "system",
-            "content": system_prompt,
-        }
-    )
-    return messages
+    return system_prompt
 
 
-def get_model_response(
-    messages: list[dict[str, str]],
+def handle_next_question(chat: Chat, question_server: QuestionServer) -> Chat:
+    """
+    Writes question data to chat as system prompt and writes question text
+    to chat interface for student to read. Return updated chat.
+    """
+    question_server.advance_question()
+    question_data = question_server.get_current_question_data()
+    question_json = json.dumps(question_data, indent=2)
+    question_message = question_server.format_question(**question_data)
+
+    system_message = f"Current question data: {question_json}"
+    chat.add_system_message(system_message)
+    print(system_message)
+    chat.add_assistant_message(question_message)
+
+    return chat
+
+
+def handle_student_response(
+    chat: Chat,
+    user_response_type: Literal["Answer", "Ask for clarification"],
     question_server: QuestionServer,
-    write_funct=print,
-) -> list[dict[str, str]]:
+    prompt: str,
+) -> Chat:
+    """
+    Adds user message to chat and then selects appropriate system prompt
+    based on user response type.
+    """
+    chat.add_user_message(prompt)
+    if user_response_type == "Answer":
+        question_server.increment_attempts()
+        system_prompt = get_system_prompt(role="assistant", prompt_type="answer")
+    elif user_response_type == "Ask for clarification":
+        question_server.increment_clarifications()
+        system_prompt = get_system_prompt(role="assistant", prompt_type="clarify")
+    else:
+        raise ValueError(f"Unknown user response type {user_response_type}")
+
+    status_message = question_server.get_attempt_and_clarification_message()
+    chat.add_assistant_message(status_message)
+    print(status_message)
+
+    chat.add_system_message(system_prompt)
+    print(system_prompt)
+    return chat
+
+
+def handle_assistant_greeting(
+    chat: Chat,
+    question_server: QuestionServer,
+) -> Chat:
+    """
+    Adds initial system prompt to chat, generates assistant
+    greeting and adds first question.
+    """
+    system_prompt = get_system_prompt(role="assistant", prompt_type="initial")
+    chat.add_system_message(system_prompt)
+    print(system_prompt)
+
+    print("Getting greeting from assistant...")
+    response = model(chat, Greeting)
+    greeting = Greeting.model_validate_json(response)
+    chat.add_assistant_message(greeting.message)
+
+    chat = handle_next_question(chat, question_server)
+    return chat
+
+
+def handle_assistant_response(
+    chat: Chat,
+    question_server: QuestionServer,
+) -> Chat:
+    """
+    Prompt model to respond to last student message.
+    Model will decide either to proceed to the next question
+    or follow up on the current question. In the former case,
+    print the next question and then return. In the latter,
+    return immediately so that the student may respond.
+    """
+
     # first get response to student's last message
-    response_json = model(Chat(messages), Response)
+    print("Getting assitant response to student input...")
+    response_json = model(chat, Response)
     response = Response.model_validate_json(response_json)
-    print(response.model_dump_json(indent=2))
 
-    write_funct(response.content)
-    messages.append(
-        {"role": "system", "content": f"Assistant responded with {response.decision}"}
-    )
-    messages.append({"role": "assistant", "content": response.content})
+    # Full JSON response stored as system message and logged to console
+    # Only "message" attribute revealed to user
+    chat.add_assistant_message(response.message)
+    system_message = f"Full assistant response in JSON format: {response_json}"
+    chat.add_system_message(system_message)
 
-    # then have model output next question, if applicable
+    # model decided to move on to next question
     if response.decision == "next_question":
-        question_server.advance_question()
-        question = question_server.get_current_question()
-        question_str = json.dumps(question)
-        messages.append({"role": "system", "content": f"Next question: {question_str}"})
+        chat = handle_next_question(chat, question_server)
 
-        question_json: Question = model(Chat(messages), Question)
-        question_object = Question.model_validate_json(question_json)
-        content = write_funct(question_object.content)
-        print(question_object.model_dump_json(indent=2))
-        messages.append({"role": "assistant", "content": content})
-        messages.append(
-            {
-                "role": "system",
-                "content": f"Assistant commentary: {model_question.comment}",
-            }
-        )
-
-    return messages
+    return chat
